4529450786f7c6e9cde4bb2f1e18a5b88249ec74
Lukas Kreussel <65088241+LLukas22@users.noreply.github.com>
2023-05-24 16:17:48 +0200

    Added conversion script

---
+++ convert.py
@@ -0,0 +1,28 @@
+from llm_rs.convert import AutoConverter
+from llm_rs.repository import Repository
+from llm_rs import ContainerType,QuantizationType,AutoQuantizer
+import sys
+import os
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python convert.py <repo_name> <output_directory> <source_repo1> <source_repo2> ....")
+        sys.exit(1)
+
+
+    repo_name = sys.argv[1]
+    output_dir = sys.argv[2]
+    targets = sys.argv[3:]
+    token = os.environ.get("HUGGINGFACE_TOKEN",None)
+    repo = Repository(repo_name,token=token)
+    print(f"Converting {'|'.join(targets)} into {repo.name}...")
+
+    container_types = [ContainerType.GGML,ContainerType.GGJT]
+
+    for target in targets:
+        converted_model = AutoConverter.convert(target, output_dir)
+        for container_type in container_types:
+            quantized = AutoQuantizer.quantize(converted_model)
+            repo.upload(quantized)
+
+    print("Done!")
