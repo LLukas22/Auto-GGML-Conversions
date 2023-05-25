2e070833adf2f5b31a281c373509538867dfefd8
Lukas Kreussel <65088241+LLukas22@users.noreply.github.com>
2023-05-25 11:32:37 +0200

    Updated to org repo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Auto-GGML-Conversions
 Automated GGML conversions to the Huggingface HUB
+
+Currently the following HF repos are created via these conversions:
+* https://huggingface.co/Rustformers/redpajama-ggml
+* https://huggingface.co/Rustformers/pythia-ggml
+* https://huggingface.co/Rustformers/dolly-v2-ggml
--- convert.py
+++ convert.py
@@ -5,19 +5,22 @@ import sys
 import os
 import logging 
 
+
+ORGANIZATION="Rustformers"
+REMOVE_CONVERTED = True
+
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
     if len(sys.argv) < 4:
         print("Usage: python convert.py <repo_name> <output_directory> <source_repo1> <source_repo2> ....")
         sys.exit(1)
-
-    REMOVE_CONVERTED = True
+        
     repo_name = sys.argv[1]
     output_dir = sys.argv[2]
     targets = sys.argv[3:]
     token = os.environ.get("HUGGINGFACE_TOKEN",None)
-    repo = Repository(repo_name,token=token)
+    repo = Repository(repo_name,ORGANIZATION,token=token)
     print(f"Converting {'|'.join(targets)} into {repo.name}...")
 
     container_types = [ContainerType.GGML,ContainerType.GGJT]
